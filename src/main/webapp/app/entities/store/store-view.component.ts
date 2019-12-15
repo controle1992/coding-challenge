5618962320cb77e8e0066d96b3fe82e0e52f7674
controle1992 <c.moussaddaq@gmail.com>
2019-12-15 15:31:08 +0100

    Add favorite stores process in the front: -Add store to favorites list. -remove store from favorites list.

--- src/main/webapp/app/entities/store/store-view.component.ts
+++ src/main/webapp/app/entities/store/store-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from 'app/shared/model/store.model';
+import { StoreService } from 'app/entities/store/store.service';
 
 @Component({
   selector: 'jhi-store-view',
@@ -7,8 +8,35 @@ import { Store } from 'app/shared/model/store.model';
 })
 export class StoreViewComponent implements OnInit, OnDestroy {
   @Input() store: Store;
+  @Input() currentAccount: any;
+  likedStore = false;
+
+  constructor(private storeService: StoreService) {}
 
   ngOnDestroy(): void {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    for (let i = 0; i < this.store.users.length; i++) {
+      if (this.store.users[i].login === this.currentAccount.login) {
+        this.likedStore = true;
+      }
+    }
+  }
+
+  addStoreToFav() {
+    this.store.users.push(this.currentAccount);
+    this.storeService.update(this.store).subscribe();
+    this.likedStore = true;
+  }
+
+  removeStoreFromFav() {
+    for (let i = 0; i < this.store.users.length; i++) {
+      if (this.store.users[i].login === this.currentAccount.login) {
+        this.store.users.splice(i, 1);
+        i--;
+      }
+    }
+    this.storeService.update(this.store).subscribe();
+    this.likedStore = false;
+  }
 }
